4ad1521b4be031800d2b66de4aecb7dd5e796ae8
furdieur <44347145+furdieur@users.noreply.github.com>
2022-11-25 02:19:52 -0500

    Further fleshed out gameplay and UI

--- js/app.js
+++ js/app.js
@@ -8,11 +8,16 @@ let app;
 let stage;
 
 let gameScene;
+let background;
+
+let targetZoomFactor = 1;
+let currentZoomFactor = 0.000001;
 
 // Game vars
 let player;
 let level;
-let boxList;
+let levelIndex = 0;
+let levelStrings;
 
 // Drawing vars
 let gridSize = 80;
@@ -26,7 +31,8 @@ let colorGray = 0xCCCCCC;
 // let colorLightGray = 0xDFDFDF;
 let colorBox = 0xa3f4ff;
 let colorPlayer = 0xffa3f4;
-let colorGoal = colorPlayer;//0x66ff85;
+let colorGoal = 0x66ff85;
+let colorWon = 0xFFD700;
 
 let colorDarknessDifference = 0x131313;
 
@@ -34,18 +40,27 @@ let colorDarknessDifference = 0x131313;
 let blurFilter = new PIXI.filters.BlurFilter();
 let antiAliasingFilter = new PIXI.filters.FXAAFilter();
 
+
+// ON LOAD
 window.onload = (e) => {
-    appDiv = document.querySelector("#app");
+    // Wait until level file has been parsed before loading the game
+    readTextFile("levels.txt", (textContent) => {
+        // Levels are separated by a comma (and return + newline)
+        levelStrings = textContent.split(",\r\n");
 
-    app = new PIXI.Application({
-        // width: 1000,
-        // height: 600
-        resizeTo: appDiv
+        appDiv = document.querySelector("#app");
+
+        app = new PIXI.Application({
+            // width: 1000,
+            // height: 600
+            resizeTo: appDiv
+        });
+        appDiv.appendChild(app.view);
+
+        app.loader.onComplete.add(setup);
+        app.loader.load();
     });
-    appDiv.appendChild(app.view);
 
-    app.loader.onComplete.add(setup);
-    app.loader.load();
 };
 
 function setup() {
@@ -53,9 +68,10 @@ function setup() {
     stage = app.stage;
 
     // Create background graphics
-    let background = new PIXI.Graphics();
+    background = new PIXI.Graphics();
     background.beginFill(0xFFFFFF);
-    background.drawRect(0, 0, app.view.width, app.view.height);
+    background.drawRect(0, 0, 10000, 10000);
+    // background.drawRect(0, 0, app.view.width, app.view.height);
     background.endFill();
 
     stage.addChild(background);
@@ -71,42 +87,100 @@ function setup() {
     blurFilter.blur = 0;
     antiAliasingFilter.resolution = 1;
 
-    // Load level
-    level = new Level();
-    level.parseLevelString(levelStrings[0]);
-    console.log(level.map);
+    restartLevel();
+    // // Load level
+    // level = new Level();
+    // level.parseLevelString(levelStrings[0]);
 
-    // Create player
-    player = new Player(level.playerSpawnRow, level.playerSpawnCol);
-    player.zIndex = 3;
-    gameScene.addChild(player);
+    // // Create player
+    // player = new Player(level.playerSpawnRow, level.playerSpawnCol);
+    // player.zIndex = 3;
+    // gameScene.addChild(player);
 
     // window.addEventListener('resize', resizeGameWindow);
     // window.addEventListener('click', resizeGameWindow);
 
-    keyboard("w").press = move;
-    keyboard("s").press = move;
-    keyboard("a").press = move;
-    keyboard("d").press = move;
-
-    app.ticker.add(gameLoop);
+    keyState("w").press = move;
+    keyState("s").press = move;
+    keyState("a").press = move;
+    keyState("d").press = move;
+    keyState("r").press = restartLevel;
+    keyState(".").press = nextLevel;
+    keyState(",").press = prevLevel;
 
     // Ensure the game window matches the screen size
     app.ticker.add(resizeGameWindow);
 }
 
-function resizeGameWindow() {
-    // console.log("Attempting resize");
+function restartLevel() {
+    // Remove all children from the gameScene
+    removeAllChildren(gameScene);
 
+    level = new Level();
+    level.parseLevelString(levelStrings[levelIndex]);
+
+    // Create player
+    player = new Player(level.playerSpawnRow, level.playerSpawnCol);
+    player.zIndex = 3;
+    gameScene.addChild(player);
+}
+
+function nextLevel() {
+    levelIndex++;
+    levelIndex = Math.min(levelIndex, levelStrings.length - 1);
+    restartLevel();
+}
+function prevLevel() {
+    levelIndex--;
+    levelIndex = Math.max(levelIndex, 0);
+    restartLevel();
+}
+
+function checkVictory() {
+    // Check every box in the level
+    for (const box of level.boxList) {
+        // If it's not on a goal, this isn't a victory
+        if (!box.onGoal) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+/// Scales the level to fit the game window, and centers it
+function resizeGameWindow() {
+
+    // Compare the aspect ratio of the viewport versus the aspect ratio of the level
     if ((app.view.width / app.view.height) > (level.mapColCount / level.mapRowCount)) {
-        // Zoom to fit level to screen height
-        gameScene.scale.set(app.view.height / gridSize / level.mapRowCount);
+        // Find zoom factor to fit level to screen height
+        targetZoomFactor = app.view.height / gridSize / level.mapRowCount;
+
+        // Center horizontally
+        gameScene.position.set((app.view.width - (level.mapColCount * gridSize * targetZoomFactor)) / 2, 0);
+
     }
     else {
-        // Zoom to fit level to screen width
-        gameScene.scale.set(app.view.width / gridSize / level.mapColCount);
+        // Find zoom factor to fit level to screen width
+        targetZoomFactor = app.view.width / gridSize / level.mapColCount;
 
+        // Center vertically
+        gameScene.position.set(0, (app.view.height - (level.mapRowCount * gridSize * targetZoomFactor)) / 2);
     }
+
+    // If there's a substantial difference between the current zoom and the target
+    if (Math.abs(targetZoomFactor - currentZoomFactor) > 0.0001) {
+        // Correct it by only a bit
+        currentZoomFactor += (targetZoomFactor - currentZoomFactor) * 0.1;
+    }
+    else {
+        currentZoomFactor = targetZoomFactor;
+    }
+
+    // Scale
+    gameScene.scale.set(currentZoomFactor);
+
+
 }
 
 function move(key) {
@@ -134,16 +208,16 @@ function move(key) {
     }
 }
 
-function gameLoop() {
-}
-
-function getGrid() {
-
+/// Update all boxes
+function updateAllBoxes() {
+    level.boxList.forEach((box) => {
+        box.update();
+    });
 }
 
 /// Returns a key object which is bound to keyup/down event listeners
 /// Code slightly modified from https://github.com/kittykatattack/learningPixi#keyboard
-function keyboard(value) {
+function keyState(value) {
     const key = {};
     key.value = value;
     key.isDown = false;
@@ -182,26 +256,33 @@ function keyboard(value) {
     return key;
 }
 
+function readTextFile(filePath, callback) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", filePath);
+    xhr.onload = () => {
+        let content = xhr.responseText;
+        // console.log(content);
+        callback(content);
+    }
+    xhr.onerror = () => {
+        console.log("Error while reading file: " + filePath);
+        // return;
+    }
+    xhr.send();
+}
+
+/// Removes all child objects from this PixiJS Object
+function removeAllChildren(pixiObj) {
+    // Adapted from https://stackoverflow.com/a/55037616
+    while (pixiObj.children[0]) {
+        pixiObj.removeChild(pixiObj.children[0]);
+    }
+}
+
 // From https://stackoverflow.com/a/8876069
-function getViewportWidth() {
-    return Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-}
-function getViewportHeight() {
-    return Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-}
-
-const levelStrings = [
-`    ####
- ####  ##
- #  # *###
-##.$#  * ##
-#   @ * * #
-#   #  *  #
-##### * * #
-    ## * ##
-    ##   #
-     ##  #
-      ####`,
-    `6#|#@$.-#|6#`
-];
-
+// function getViewportWidth() {
+//     return Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+// }
+// function getViewportHeight() {
+//     return Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+// }
--- js/classes.js
+++ js/classes.js
@@ -38,19 +38,71 @@ class GoalTile extends PIXI.Graphics {
 class Box extends PIXI.Graphics {
     constructor(row, col) {
         super();
-        this.beginFill(colorBox);
-        this.drawRoundedRect(largerPadding, largerPadding, gridSize - largerPadding * 2, gridSize - largerPadding * 2, cornerRadius);
-        this.endFill();
 
         this.zIndex = 2;
 
+        this.setPos(row, col);
+    }
+
+    update() {
+        this.onGoal = level.isLoaded && level.map[this.row][this.col] == ".";
+
+        this.clear();
+        if (this.onGoal) {
+            if (checkVictory()) {
+                this.beginFill(colorWon);
+            }
+            else {
+                this.beginFill(colorGoal);
+            }
+        }
+        else {
+            this.beginFill(colorBox);
+        }
+        this.drawRoundedRect(largerPadding, largerPadding, gridSize - largerPadding * 2, gridSize - largerPadding * 2, cornerRadius);
+        this.endFill();
+    }
+
+    setPos(row, col) {
+        this.row = row;
+        this.col = col;
         this.x = col * gridSize;
         this.y = row * gridSize;
+
+        this.update();
+    }
+
+    isWallInPath(rows, cols) {
+        return level.map[this.row + rows][this.col + cols] == "#";
+    }
+
+    getBoxInPath(rows, cols) {
+        // Check every box in the level
+        for (const box of level.boxList) {
+            // Is there a box (that isn't us) where we're attempting to move?
+            if (box.row == this.row + rows && box.col == this.col + cols && box != this) {
+                return box;
+            }
+        }
+
+        return null;
+    }
+
+    /// Moves the box the provided distance if unobstructed, returning true if success
+    pushBox(rows, cols) {
+        // If there's no wall or box in the way
+        if (!this.isWallInPath(rows, cols) && !this.getBoxInPath(rows, cols)) {
+            // Move
+            this.setPos(this.row + rows, this.col + cols);
+
+            return true;
+        }
+
+        return false;
     }
 
 
 }
-
 
 class Player extends PIXI.Graphics {
     constructor(row, col) {
@@ -70,26 +122,56 @@ class Player extends PIXI.Graphics {
         this.y = row * gridSize;
     }
 
+    /// Move the player the provided number of rows and columns, pushing any unobstructed boxes in the way
     move(rows, cols) {
+        // If this is a wall, don't move
         if (level.map[this.row + rows][this.col + cols] == "#") {
             return;
         }
-        this.row += rows;
-        this.col += cols;
-        this.setPos(this.row, this.col);
+
+        // Check every box in the level
+        for (const box of level.boxList) {
+            // Is there a box where we're attempting to move?
+            if (box.row == this.row + rows && box.col == this.col + cols) {
+                // If the box was pushed successfully
+                if (box.pushBox(rows, cols)) {
+                    // Check to see if you won
+                    checkVictory();
+                    updateAllBoxes();
+
+                    // Move into where it was pushed
+                    break;
+                }
+                // Otherwise, the box was not pushed successfully
+                else {
+                    // Do not move
+                    return;
+                }
+            }
+        }
+
+        // Move
+        this.setPos(this.row + rows, this.col + cols);
     }
 }
 
 class Level {
     constructor() {
+        this.boxList = [];
+        this.isLoaded = false;
     }
 
     /// Parses a run-length encoded standard Sokoban level string into a 2D map
     parseLevelString(levelString) {
+        // Level is not loaded
+        this.isLoaded = false;
+
         // Level 2D array
         this.map = [];
 
-        boxList = [];
+        // Clear the level of all boxes
+        this.boxList.forEach(b => gameScene.removeChild(b));
+        this.boxList = [];
 
         // Standardize the string to use hyphens instead of spaces or underscores
         levelString = levelString.replaceAll(" ", "-");
@@ -131,6 +213,8 @@ class Level {
                         let rowIndex = j; // the current row number in the text file
                         let colIndex = row.length; // next available index in the row
 
+                        let newBox;
+
                         switch (symbol) {
                             // Empty space
                             case "-":
@@ -140,66 +224,71 @@ class Level {
 
                             // Wall
                             case "#":
-                                // Add wall tile
-                                gameScene.addChild(new WallTile(rowIndex, colIndex));
-
                                 // Put a wall here in the level map
                                 row.push("#");
+
+                                // Add wall tile
+                                gameScene.addChild(new WallTile(rowIndex, colIndex));
                                 break;
 
                             // Player
                             case "@":
+                                // Put an empty space here in the level map
+                                row.push("-");
+
                                 // Set player spawn location
                                 this.playerSpawnRow = rowIndex;
                                 this.playerSpawnCol = colIndex;
-
-                                // Put an empty space here in the level map
-                                row.push("-");
                                 break;
 
                             // Player on Goal Square
                             case "+":
+                                // Put a goal here in the level map
+                                row.push(".");
+
+                                // Add a goal tile
+                                gameScene.addChild(new GoalTile(rowIndex, colIndex));
+
                                 // Set player spawn location
                                 this.playerSpawnRow = rowIndex;
                                 this.playerSpawnCol = colIndex;
-
-                                // Also add a goal tile
-                                gameScene.addChild(new GoalTile(rowIndex, colIndex));
-
-                                // Put a goal here in the level
-                                row.push(".");
                                 break;
 
                             // Box
                             case "$":
-                                // Add a box
-                                gameScene.addChild(new Box(rowIndex, colIndex));
-
                                 // Put an empty space here in the level map
                                 row.push("-");
+
+                                // Add a box to the box list
+                                newBox = new Box(rowIndex, colIndex);
+                                newBox.onGoal = false;
+                                this.boxList.push(newBox);
+                                gameScene.addChild(newBox);
                                 break;
 
                             // Box on goal square
                             case "*":
-                                // Add a box
-                                gameScene.addChild(new Box(rowIndex, colIndex));
+                                // Put a goal here in the level map
+                                row.push(".");
 
                                 // Also add a goal tile
                                 gameScene.addChild(new GoalTile(rowIndex, colIndex));
 
-                                // Put a goal here in the level map
-                                row.push(".");
+                                // Add a box to the box list
+                                newBox = new Box(rowIndex, colIndex);
+                                newBox.onGoal = true;
+                                this.boxList.push(newBox);
+                                gameScene.addChild(newBox);
                                 break;
 
                             // Goal
                             case ".":
-                                // Add a goal tile
-                                gameScene.addChild(new GoalTile(rowIndex, colIndex));
-
                                 // Put a goal here in the level map
                                 row.push(".");
-                                break;
 
+                                // Add a goal tile
+                                gameScene.addChild(new GoalTile(rowIndex, colIndex));
+                                break;
                         }
                     }
 
@@ -213,8 +302,14 @@ class Level {
             this.map.push(row);
         }
 
+        // Find the column count (tile width) of this level
         this.mapColCount = 0;
-        this.map.forEach((e) => { if (e.length > this.mapColCount) this.mapColCount = e.length });
+        this.map.forEach((row) => { if (row.length > this.mapColCount) this.mapColCount = row.length });
+
+        // Mark level as loaded
+        this.isLoaded = true;
+
+        updateAllBoxes();
     }
 
 }
